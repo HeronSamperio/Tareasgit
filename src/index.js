62b5122def444c92087d6238e5d7b267dd9728b3
HeronSamperio <79339532+HeronSamperio@users.noreply.github.com>
2021-02-28 23:53:35 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Card/>
+  </StrictMode>,
+  rootElement
+);
